2ef96ed737402d482cecdc0ecfb47a077084b7f4
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2021-04-10 18:32:29 +0000

    Moved scraping process into a function.

--- Scrape50.py
+++ Scrape50.py
@@ -2,40 +2,52 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-link = "https://www.lookfantastic.com/health-beauty/make-up/eyes/mascaras.list?pageNumber=1&sortOrder=salesRank"
+foundation_link = "https://www.lookfantastic.com/health-beauty/make-up/complexion/foundation-makeup.list"
+mascara_link = "https://www.lookfantastic.com/health-beauty/make-up/eyes/mascaras.list"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
 
-data = []
-page = requests.get(link).text
-soup = BeautifulSoup(page, 'html.parser')
-product_list = soup.find_all('li', {"class": "productListProducts_product"})
+def get_top_products(category, link, headers):
+    """Scrape info from top products page of specified category link"""
 
-# Use individual elements to get product name & price
-for product in product_list:
-    try:
-        name = product.find('h3', {"class": "productBlock_productName"}).text.replace('\n', "")
-    except:
-        name = None
+    # Modify link to sort by top sales rank
+    link_modified = link + "?pageNumber=1&sortOrder=salesRank"
 
-    try:
-        price = product.find('span', {"class": "productBlock_priceValue"}).get('content').replace('\n', "")
-    except:
-        price = None
+    data = []
+    page = requests.get(link_modified, headers=headers).text
+    soup = BeautifulSoup(page, 'html.parser')
+    product_list = soup.find_all('li', {"class": "productListProducts_product"})
 
-    try:
-        brand = product.find('span', {"class": "js-enhanced-ecommerce-data"}).get('data-product-brand').replace('\n', "")
-    except:
-        brand = None
+    for product in product_list:
+        product_data = product.find('span', {"class": "js-enhanced-ecommerce-data"})
 
-    try:
-        product_id = product.find('span', {"class": "js-enhanced-ecommerce-data"}).get('data-product-master-product-id').replace('\n', "")
-    except:
-        product_id = None
+        try:
+            name = product_data.get('data-product-title').replace('\n', "")
+        except:
+            name = None
 
-    mascara = {'name': name, 'price': price, 'brand': brand, 'product_id': product_id}
-    data.append(mascara)
+        try:
+            price = product_data.get('data-product-price').replace('\n', "")
+        except:
+            price = None
 
-df = pd.DataFrame(data)
-df.to_csv('data_output.csv')
+        try:
+            brand = product_data.get('data-product-brand').replace('\n', "")
+        except:
+            brand = None
+
+        try:
+            product_id = product_data.get('data-product-master-product-id').replace('\n', "")
+        except:
+            product_id = None
+
+        item = {'category': category, 'name': name, 'price': price, 'brand': brand, 'product_id': product_id}
+        data.append(item)
+
+    return data
+
+foundations = get_top_products('Foundation', foundation_link, headers)
+
+df = pd.DataFrame(foundations)
+df.to_csv('data_output.csv', index=False)
 
 print(df)
