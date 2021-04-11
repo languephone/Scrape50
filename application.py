33e2565c560cad9ea73567369b9b326782a1a350
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2021-04-11 16:47:37 +0000

    Created SQL database connection.

--- application.py
+++ application.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
 import csv
+import sqlite3
 
 app = Flask(__name__)
 
 def gbp(value):
     """Format value as GBP."""
     return f"£{value:,.2f}"
+    
+# Create a database connection to a SQLite database
+db = sqlite3.connect('products.db')
+cur = db.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS products(
+    id integer PRIMARY KEY,
+    name text NOT NULL,
+    price real NOT NULL,
+    site text NOT NULL,
+    date text NOT NULL,
+    site_id text NOT NULL);""")
+db.commit()
+db.close()
+
 
 # Custom filter
 app.jinja_env.filters["gbp"] = gbp
