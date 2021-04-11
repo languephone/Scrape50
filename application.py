b979a280411cce046080d8834153c9529727b3bb
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2021-04-11 12:50:52 +0000

    Added Bootstrap stylesheet link.

--- application.py
+++ application.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world!"
+    return render_template("index.html")
+
+@app.route("/<string:category>")
+def category(category):
+
+    products = []
+    # Import product info from csv file into list
+    filename = 'data_output.csv'
+    with open(filename) as f:
+        dict_reader = csv.DictReader(f)
+        for row in dict_reader:
+            if row['category'] == category:
+                products.append(row)
+    
+
+
+    return render_template("index.html", category=category, products=products)
