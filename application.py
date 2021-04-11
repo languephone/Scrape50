3eb09a339df7398b54881a558e4220e22cc623ea
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2021-04-11 13:51:04 +0000

    Added checkboxes to let user choose which brands included.  Not yet functional.

--- application.py
+++ application.py
@@ -3,6 +3,13 @@ import csv
 
 app = Flask(__name__)
 
+def gbp(value):
+    """Format value as GBP."""
+    return f"£{value:,.2f}"
+
+# Custom filter
+app.jinja_env.filters["gbp"] = gbp
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -17,9 +24,8 @@ def category(category):
         dict_reader = csv.DictReader(f)
         for row in dict_reader:
             if row['category'] == category:
+                row['price'] = float(row['price'])
                 products.append(row)
     products.sort(key = lambda i: float(i['price']))
-
-
 
     return render_template("index.html", category=category, products=products)
