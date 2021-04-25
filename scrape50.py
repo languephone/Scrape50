aafeed84cc46e9ab089b487f6761ffe4f4940ddc
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2021-04-25 19:43:15 +0000

    Completed HOF scraper as new class.

--- scrape50.py
+++ scrape50.py
@@ -10,6 +10,14 @@ class Scraper:
     def __init__(self):
         """Initialise nothing.  Does this make sense?"""
 
+    def loop_through_categories(self):
+        """Loop through specified categories and call get_top_products()."""
+        for category in self.categories:
+            self.get_top_products(category)
+
+    def clean_all_products(self):
+        for product in self.data:
+            product['name'] = self.clean_product_name(products['name'])
 
     def clean_product_name(self, product_name):
         """Remove colour and/or shade information from product name"""
@@ -19,7 +27,6 @@ class Scraper:
             product_name = product_name.replace(replacement, "").strip()
 
         return product_name
-
 
     def write_to_sql(self):
         """Take a list of dictionaries of scraped data, and write to a SQLite database"""
@@ -49,7 +56,7 @@ class Scraper:
         """Take a list of dictionaries of scraped data, and write to a CSV file"""
 
         df = pd.DataFrame(self.data)
-        df.to_csv('data_output_classes.csv', index=False)
+        df.to_csv(f'data_output_classes_{self.site}.csv', index=False)
 
 
 class LookFantastic(Scraper):
@@ -111,15 +118,74 @@ class LookFantastic(Scraper):
             item = {'category': category, 'name': name, 'price': price, 'brand': brand, 'product_id': product_id, 'image_link': image_link, 'site_id': self.site}
             self.data.append(item)
 
-    def loop_through_categories(self):
-        """Loop through specified categories and call get_top_products()."""
-        for category in self.categories:
-            self.get_top_products(category)
+class HouseOfFraser(Scraper):
+    "A class to scrape from the House of Fraser website."
 
-    def clean_all_products(self):
-        for product in self.data:
-            product['name'] = self.clean_product_name(products['name'])
+    def __init__(self):
+        """Initialize attributes of the parent class."""
+        super().__init__()
+        self.site = "House of Fraser"
+        self.base_link = "https://www.houseoffraser.co.uk/beauty/"
+        self.sorting_modifier = ""
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
+        # TODO find replacement for hard coding category links
+        self.categories = {
+            'foundation': "foundations",
+            'mascara': "mascaras"
+            }
+        self.data = []
 
-lf = LookFantastic()
-lf.loop_through_categories()
-lf.write_to_csv()
+    def get_top_products(self, category):
+        """Scrape info from top products page of specified category link"""
+
+        # Modify link to sort by top sales rank
+        link_modified = self.base_link + self.categories[category] + self.sorting_modifier
+
+
+        page = requests.get(link_modified, headers=self.headers).text
+        soup = BeautifulSoup(page, 'html.parser')
+        product_grid = soup.find("ul",{"class":"s-productscontainer2"})
+        product_list = product_grid.find_all("li", recursive=False)
+
+        for product in product_list:
+
+            try:
+                name = product.get('li-name').replace('\n', "")
+            except:
+                name = None
+
+            try:
+                price = product.get('li-price').replace('\n', "").replace("£", "")
+            except:
+                price = None
+
+            try:
+                brand = product.get('li-brand').replace('\n', "")
+            except:
+                brand = None
+
+            try:
+                product_id = product.get('li-productid').replace('\n', "")
+            except:
+                product_id = None
+
+            try:
+                image_link = product.find('img').get('src').replace('\n', "")
+            except:
+                try:
+                    image_link = product.find('img').get('data-original').replace('\n', "")
+                except:
+                    image_link = None
+
+            item = {'category': category, 'name': name, 'price': price, 'brand': brand, 'product_id': product_id, 'image_link': image_link, 'site_id': self.site}
+            self.data.append(item)
+
+
+
+# lf = LookFantastic()
+# lf.loop_through_categories()
+# lf.write_to_csv()
+
+hof = HouseOfFraser()
+hof.loop_through_categories()
+hof.write_to_csv()
