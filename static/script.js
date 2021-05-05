77faf12aba7e8a1ea63fcaa719e04ac77b36424a
languephone <73309519+languephone@users.noreply.github.com>
2021-05-05 18:35:42 +0100

    Added unused vanilla js AJAX request to script.js.

--- static/script.js
+++ static/script.js
@@ -2,3 +2,22 @@ function hide_card(card)
 {
     card.style.display = 'none';
 }
+
+// let input = document.querySelector('input');
+// input.addEventListener('keyup', function() {
+//     $.get('/search?q=' + input.value, function(brands) {
+//         let html = '';
+//         for (let item in brands)
+//         {
+//             let brand = brands[item].brand;
+//             let scrapedate = brands[item].scrapedate;
+//             let site = brands[item].site;
+//             html += '<tr>'
+//                 + '<td>' + brand + '</td>'
+//                 + '<td>' + scrapedate + '</td>'
+//                 + '<td>' + site + '</td>'
+//                 + '</tr>'
+//         }
+//         document.querySelector('tbody').innerHTML = html;
+//     });
+// });
